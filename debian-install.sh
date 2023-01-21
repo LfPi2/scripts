ff15a6747be3f52eb2cd7ca081d87885aa242af0
LfPi2 <lfpi2.00000001@gmail.com>
2023-01-21 15:43:23 +0000

    Debian install script added

---
+++ debian-install.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+if [ $(id -u) = "0" ]; then
+	echo 'You should not run the script as root'
+	exit
+fi
+
+REPO_PATH="$(pwd)"
+
+cd ~
+
+sudo apt-get -y update
+sudo apt-get -y install git gcc make man-db unzip curl wget
+sudo apt-get -y install xorg pulseaudio
+sudo apt-get -y install libx11-dev libxft-dev libxinerama-dev dbus-x11
+sudo apt-get -y install xdg-user-dirs feh acipi picom htop ssh bat
+sudo apt-get -y install neovim kitty nodejs npm flatpak
+sudo apt-get -y install firefox-esr thunar pavucontrol mpv
+sudo apt-get -y install neofetch cowsay cmatrix cbonsai lolcat cava
+
+xdg-user-dirs-update
+mkdir ~/code
+mkdir ~/repos
+
+cd ~/repos
+
+git clone https://github.com/LfPi2/dwm
+git clone https://github.com/LfPi2/dmenu
+git clone https://github.com/LfPi2/Nordic
+
+cd dwm
+make
+sudo make clean install
+cd ../dmenu
+make
+sudo make clean install
+cd ..
+sudo cp -r Nordic /usr/share/themes
+
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+cd $REPO_PATH
+
+curl -sS https://starship.rs/install.sh | sudo sh
+cp wallpaper.png ~/Pictures
+sudo unzip font-mononoki.zip -d /usr/share/fonts
+
+cd ~
+
+git clone https://github.com/LfPi2/dotfiles
