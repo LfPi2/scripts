6f249f3ef279028508583eefb99d02f0844a00bd
LfPi2 <lfpi2.00000001@gmail.com>
2023-01-22 21:07:05 +0000

    Corrected from acipi acpi

--- debian-install.sh
+++ debian-install.sh
@@ -13,7 +13,7 @@ sudo apt-get -y update
 sudo apt-get -y install git gcc make man-db unzip curl wget
 sudo apt-get -y install xorg pulseaudio
 sudo apt-get -y install libx11-dev libxft-dev libxinerama-dev dbus-x11
-sudo apt-get -y install xdg-user-dirs feh acipi picom htop ssh bat
+sudo apt-get -y install xdg-user-dirs feh acpi picom htop ssh bat
 sudo apt-get -y install neovim kitty nodejs npm flatpak
 sudo apt-get -y install firefox-esr thunar pavucontrol mpv
 sudo apt-get -y install neofetch cowsay cmatrix cbonsai lolcat cava
@@ -26,7 +26,7 @@ cd ~/repos
 
 git clone https://github.com/LfPi2/dwm
 git clone https://github.com/LfPi2/dmenu
-git clone https://github.com/LfPi2/Nordic
+git clone https://github.com/EliverLara/Nordic
 
 cd dwm
 make
@@ -47,4 +47,4 @@ sudo unzip font-mononoki.zip -d /usr/share/fonts
 
 cd ~
 
-git clone https://github.com/LfPi2/dotfiles
+git clone --separate-git-dir .dotfiles https://github.com/LfPi2/dotfiles ./
